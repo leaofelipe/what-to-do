fdfea4ec00a366d75d312ee13240f14dc180c64a
Felipe Leão <felipe@felipeleao.com>
2018-05-09 22:55:00 -0300

    chore: Install babel

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
-'use strict';
-
 console.log('App.js is running.');
 
 // JSX - Javascript XML
-var template = React.createElement(
-  'p',
-  null,
-  'This is JSX sample.'
-);
+var template = <p>This is JSX sample.</p>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
