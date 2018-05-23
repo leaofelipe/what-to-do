ef032a0d4bcc3bb2346a189066d6a360b65a1663
Felipe Leão <felipe@felipeleao.com>
2018-05-22 22:18:39 -0300

    tip: Just a simple render example

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,45 +2,77 @@
 
 console.log('App.js is running.');
 
-var app = {
-  name: 'What to do APP'
+var count = 0;
+var addOne = function addOne() {
+  count++;
+  renderCounterApp();
+  console.log('+1');
+  /* count++; */
 };
-var restaurants = ['McDonalds', 'Subway'];
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.name
-  ),
-  React.createElement(
-    'p',
-    null,
-    restaurants.length > 0 ? 'Options:' : 'No options.'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    restaurants[0] && React.createElement(
-      'li',
-      null,
-      restaurants[0]
-    ),
-    restaurants[1] && React.createElement(
-      'li',
-      null,
-      restaurants[1]
-    ),
-    restaurants[2] && React.createElement(
-      'li',
-      null,
-      restaurants[2]
-    )
-  )
-);
+var minusOne = function minusOne() {
+  count--;
+  renderCounterApp();
+  console.log('-1');
+};
+
+var reset = function reset() {
+  count = 0;
+  renderCounterApp();
+  console.log('Reset.');
+};
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'Reset'
+    )
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
+
+/* const app = { */
+/*   name: 'What to do APP' */
+/* }; */
+/* let restaurants = ['McDonalds', 'Subway']; */
+
+/* const template = ( */
+/*   <div> */
+/*     <h1>{app.name}</h1> */
+/*     <p>{restaurants.length > 0 ? 'Options:' : 'No options.'}</p> */
+/*     <ol> */
+/*       {restaurants[0] && <li>{restaurants[0]}</li>} */
+/*       {restaurants[1] && <li>{restaurants[1]}</li>} */
+/*       {restaurants[2] && <li>{restaurants[2]}</li>} */
+/*     </ol> */
+/*   </div> */
+/* ); */
+
+/* const appRoot = document.getElementById('app'); */
+
+/* ReactDOM.render(template, appRoot); */
--- src/app.js
+++ src/app.js
@@ -1,22 +1,60 @@
 console.log('App.js is running.');
 
-const app = {
-  name: 'What to do APP'
+let count = 0;
+const addOne = () => {
+  count++;
+  renderCounterApp();
+  console.log('+1');
+  /* count++; */
 };
-let restaurants = ['McDonalds', 'Subway'];
 
-const template = (
-  <div>
-    <h1>{app.name}</h1>
-    <p>{restaurants.length > 0 ? 'Options:' : 'No options.'}</p>
-    <ol>
-      {restaurants[0] && <li>{restaurants[0]}</li>}
-      {restaurants[1] && <li>{restaurants[1]}</li>}
-      {restaurants[2] && <li>{restaurants[2]}</li>}
-    </ol>
-  </div>
-);
+const minusOne = () => {
+  count--;
+  renderCounterApp();
+  console.log('-1');
+};
+
+const reset = () => {
+  count = 0;
+  renderCounterApp();
+  console.log('Reset.');
+};
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+const renderCounterApp = () => {
+ const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
+
+/* const app = { */
+/*   name: 'What to do APP' */
+/* }; */
+/* let restaurants = ['McDonalds', 'Subway']; */
+
+/* const template = ( */
+/*   <div> */
+/*     <h1>{app.name}</h1> */
+/*     <p>{restaurants.length > 0 ? 'Options:' : 'No options.'}</p> */
+/*     <ol> */
+/*       {restaurants[0] && <li>{restaurants[0]}</li>} */
+/*       {restaurants[1] && <li>{restaurants[1]}</li>} */
+/*       {restaurants[2] && <li>{restaurants[2]}</li>} */
+/*     </ol> */
+/*   </div> */
+/* ); */
+
+/* const appRoot = document.getElementById('app'); */
+
+/* ReactDOM.render(template, appRoot); */
+
